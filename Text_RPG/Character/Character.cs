8fba9d18546dc4ec06540e8a759b438f11a13129
seo600 <conrad270@naver.com>
2024-09-25 15:45:29 +0900

    (Init) 프로젝트 초기 생성
    프로젝트 생성

---
+++ Text_RPG/Character/Character.cs
@@ -0,0 +1,64 @@
+﻿namespace TextRPG
+{
+    class Character //캐릭터 정보
+    {
+        public int Lv { get; set; } = 1;
+        public string Name { get; set; }
+        public int Atk { get; set; }
+        public int? AtkE { get; set; } = null;
+        public int Def { get; set; }
+        public int? DefE { get; set; } = null;
+        public int Hp { get; set; }
+        public int Gold { get; set; } = 10000;
+        
+
+        public List<Item> inventory = new List<Item>();
+        public string item;
+        string ename;
+
+        private int count = 0;
+
+
+        public virtual void Stat()
+        {
+            Console.WriteLine($"Lv. {Lv}");
+            Console.WriteLine($"이름 ( {Name} )");
+            Console.WriteLine($"공격력 : {Atk}");
+            Console.WriteLine($"방어력 : {Def}");
+            Console.WriteLine($"체 력 : {Hp}");
+            Console.WriteLine($"Gold :  {Gold}G\n");
+        }
+
+        public string ItemString(int i)
+        {
+            if (inventory[i].itemE == false)
+            {
+                if (inventory[i].atk > 0)
+                {
+                    item = "-  " + (i + 1) + " " + inventory[i].itemname.PadRight(12) + "  ㅣ " + "공격력 +" + inventory[i].atk + " ㅣ " + inventory[i].explanation;
+                }
+                else
+                {
+                    item = "-  " + (i + 1) + " " + inventory[i].itemname.PadRight(12) + "  ㅣ " + "방어력 +" + inventory[i].def + " ㅣ " + inventory[i].explanation;
+                }
+            }
+            else if (inventory[i].itemE == true)
+            {
+                if (inventory[i].atk > 0)
+                {
+
+                    item = "-  " + (i + 1) + " [E]" + inventory[i].itemname.PadRight(12) + "  ㅣ " + "공격력 +" + inventory[i].atk + " ㅣ " + inventory[i].explanation;
+                }
+                else
+                {
+                    item = "-  " + (i + 1) + " [E]" + inventory[i].itemname.PadRight(12) + "  ㅣ " + "방어력 +" + inventory[i].def + " ㅣ " + inventory[i].explanation;
+                }
+            }
+
+            return item;
+        }
+        
+    }
+
+}
+
---
+++ Text_RPG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace TextRPG
+{
+    internal class Program
+    {
+        public static Character character = new Character();
+        public static Village village = new Village();
+        static void Main(string[] args)
+        {
+            FirstScreen screen = new();
+            screen.InputName();
+
+        }
+
+    }
+
+}
+
